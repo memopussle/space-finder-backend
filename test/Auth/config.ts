94dac1c4bfe3cc5e58812eb633686ac87826ee9a
thugiang <xanhtham.cuc@gmail.com>
2022-10-11 00:14:45 +1300

    getting JWT tokens with AWS amplify

---
+++ test/Auth/config.ts
@@ -0,0 +1,7 @@
+export const config = {
+  REGION: "ap-southeast-2",
+  USER_POOL_ID: "ap-southeast-2_nqckfyacF",
+  APP_CLIENT_ID: "616l25voud9fnspf81h757i1mo",
+  TEST_USER_NAME: "xanhtham.cuc",
+  TEST_USER_PASSWORD: "Pussles123!",
+};
